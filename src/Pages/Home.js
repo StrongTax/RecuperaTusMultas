e41f59a62b6c0876fb1d68f49310188625e5bf45
StrongTax <nicohdzlo@hotmail.com>
2022-05-13 00:02:24 +0000

    Forth Commit

--- src/Pages/Home.js
+++ src/Pages/Home.js
@@ -1,17 +1,18 @@
 //This is home page, It will contains all the sections require in this page.
-import React  from 'react';
-//Import all the require sections here
+import React from "react";
 import HeroSection from "../Sections/Hero/index";
 import About from "../Sections/About/index";
 import Services from "../Sections/Services/index";
 import Testimonials from "../Sections/Testimonials/index";
 import Contact from "../Sections/Contact/index";
-import BlogPost from '../components/BlogPost/BlogPost.js';
+import BlogPost from "../components/BlogPost/BlogPost.js";
 
 import styled from "styled-components";
 
 const Container = styled.div`
   display: flex;
+  overflow-x: hidden;
+  overflow-y: hidden;
   flex-direction: column;
   justify-content: center;
   align-items: center;
@@ -23,11 +24,11 @@ const Home = () => {
     <Container>
       <HeroSection />
       <About />
-      <BlogPost/>
+      <BlogPost />
       {/*<Card1/>*/}
       <Services />
-      <Testimonials/>
-     <Contact />
+      <Testimonials />
+      <Contact />
     </Container>
   );
 };
