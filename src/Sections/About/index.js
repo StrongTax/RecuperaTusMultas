69b689db8e25474ef3bc71efb25101c11edaa2e2
StrongTax <nicohdzlo@hotmail.com>
2022-06-09 05:38:45 +0000

    1.3

--- src/Sections/About/index.js
+++ src/Sections/About/index.js
@@ -4,6 +4,7 @@ import wave from "../../assets/waves.svg";
 import rocket from "../../assets/rocket image.svg";
 import human from "../../assets/human.svg";
 import hand from "../../assets/hand.svg";
+import Clients from "../../components/Clients/index.jsx"
 
 const move = keyframes`
 0% { transform: translateY(-5px)         }
@@ -15,13 +16,19 @@ const AboutSection = styled.section`
   overflow-x: hidden;
 
   overflow-y: hidden;
-
+  padding-top: 22rem;
   width: 100%;
   position: relative;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+  
+  @media screen and (min-width: 280px) and (max-width: 1080px) {
+    padding-top: 12rem;
+
+  }
+
 `;
 const Waves = styled.img`
   width: 101%;
@@ -41,17 +48,16 @@ const Hand = styled.div`
 
 const Main = styled.div`
   margin: 0 15rem;
-  margin-top: 15rem;
-  display: flex;
+s  display: flex;
   justify-content: center;
   flex-direction: column;
   @media only Screen and (max-width: 64em) {
     margin: 0 calc(5rem + 5vw);
-    margin-top: 10rem;
+    margin-top: 1.5rem;
   }
   @media only Screen and (max-width: 40em) {
     align-items: center;
-    margin: 3rem calc(3rem + 3vw);
+    margin: 1rem calc(3rem + 3vw);
   }
 `;
 
@@ -127,6 +133,7 @@ const About = () => {
   return (
     <AboutSection id="about">
       <Waves src={wave} alt="" />
+      <Clients/>
       <div data-aos="fade-right" data-aos-duration="2200">
         <Hand>
           <img src={hand} alt="" />
--- src/components/Clients/index.jsx
+++ src/components/Clients/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
-import clients1 from "../assets/clients1.png";
+import '../../components/Clients/index.scss'
+import clients1 from "../../assets/clients1.png";
 /*import clients2 from "../assets/clients2.png";
 import clients3 from "../assets/clients3.png";
 import clients4 from "../assets/clients4.png";
@@ -7,7 +8,7 @@ import clients5 from "../assets/clients5.png";
 */
 
 export default function Clients() {
-  const data = [clients1, clients1, clients1, clients1, clients1];
+  const data = [clients1,clients1,clients1,clients1,clients1, clients1, clients1, clients1, clients1];
   return (
     <div className="clients">
       <div className="container">
